ce7fd5b39f92d0b7eca7909fa163611e3a15bfa1
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-05-12 13:09:29 +0200

    Continuamos

--- src/main/java/Controller/Controlador.java
+++ src/main/java/Controller/Controlador.java
@@ -1,5 +1,80 @@
 package Controller;
 
+import Interfaces.IUtils;
+import Interfaces.IVista;
+import Utils.Utils;
+import Vistas.Vistas;
+
 public class Controlador {
 	
+	IVista vista = new Vistas();
+	IUtils util = new Utils();
+	
+	private void SwitchMain(int op) {
+		switch (op) {
+		case 1:
+			SwitchMenuPlayer(util.opcMenu5());
+			break;
+		case 2:
+			SwitchMenuTeam(util.opcMenu5());
+			break;
+		case 3:
+			SwitchMenuTorneos(util.opcMenu5());
+			break;
+		case 4:
+			util.print("Gracias por utilizar nuestro programa");
+			break;
+		}
+	}
+	
+	private void SwitchMenuPlayer(int op) {
+		switch (op) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			vista.showMainMenu();
+			SwitchMain(util.opcMenu3());
+			break;
+		}
+	}
+	
+	private void SwitchMenuTeam(int op) {
+		switch (op) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			vista.showMainMenu();
+			SwitchMain(util.opcMenu3());
+			break;
+		}
+	}
+	
+	private void SwitchMenuTorneos(int op) {
+		switch (op) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			vista.showMainMenu();
+			SwitchMain(util.opcMenu3());
+			break;
+		}
+	}
 }
--- src/main/java/Vistas/Vistas.java
+++ src/main/java/Vistas/Vistas.java
@@ -1,6 +1,8 @@
 package Vistas;
 
-public class Vistas {
+import Interfaces.IVista;
+
+public class Vistas implements IVista{
 	public void showMainMenu() {
 		System.out.println("1. Jugadores");
 		System.out.println("2. Equipos");
@@ -8,7 +10,7 @@ public class Vistas {
 		System.out.println("4. Salir");
 	}
 	
-	public void ShowMainPlayer() {
+	public void showMainPlayer() {
 		System.out.println("1. Crear jugador");
 		System.out.println("2. Eliminar jugador");
 		System.out.println("3. Editar jugador");
@@ -16,7 +18,7 @@ public class Vistas {
 		System.out.println("5. Volver");
 	}
 	
-	public void ShowMainTeam() {
+	public void showMainTeam() {
 		System.out.println("1. Crear equipo");
 		System.out.println("2. Eliminar equipo");
 		System.out.println("3. Editar equipo");
@@ -24,10 +26,16 @@ public class Vistas {
 		System.out.println("5. Volver");
 	}
 	
-	public void ShowMainTorneos() {
+	public void showMainTorneos() {
 		System.out.println("1. Crear torneo");
 		System.out.println("2. Eliminar torneo");
 		System.out.println("3. Apuestas");
 		System.out.println("4. Ver torneo");
+		System.out.println("5. Volver");
 	}
+	
+	public void separar() {
+		System.out.println("---------------------------------------");
+	}
+
 }
